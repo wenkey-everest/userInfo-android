ed50807698d11fa7c14e45e0e2631f619a1216df
wenkey <muni.v.g@everest.engineering>
2022-02-27 14:02:59 +0530

    Added custom splash screen

---
+++ app/src/main/java/com/everest/userinfo/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.everest.userinfo
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity :AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+
+    }
+}
