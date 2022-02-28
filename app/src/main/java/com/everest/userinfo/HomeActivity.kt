fd182a4850da28cfeda05878629b6427c873dd88
wenkey <muni.v.g@everest.engineering>
2022-02-28 08:51:10 +0530

    Added buttons to the summery card for navigation

--- app/src/main/java/com/everest/userinfo/HomeActivity.kt
+++ app/src/main/java/com/everest/userinfo/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.everest.userinfo
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.everest.userinfo.databinding.ActivityHomeBinding
@@ -17,8 +19,8 @@ class HomeActivity : AppCompatActivity() {
         setContentView(view)
         binding.validateButton.setOnClickListener {
             if(isValidate()) {
-                Toast.makeText(this, "All fields are validated", Toast.LENGTH_LONG)
-                    .show()
+               onClick()
+
             }
         }
 
@@ -31,5 +33,27 @@ class HomeActivity : AppCompatActivity() {
                && TextFieldValidation().validatePinCode(binding, this)
                && TextFieldValidation().validateAddress(binding, this)
     }
+
+
+    private fun onClick(){
+        binding.editForm.visibility = View.GONE
+        binding.summeryCard.visibility = View.VISIBLE
+        binding.nameId.text = binding.userName.editText?.text.toString()
+        binding.emailId.text = binding.email.editText?.text.toString()
+        binding.phoneNumberId.text = binding.phoneNumber.editText?.text.toString()
+        binding.pinCodeId.text = binding.pinCode.editText?.text.toString()
+        binding.addressId.text = binding.address.editText?.text.toString()
+
+        binding.confirmButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.cancelButton.setOnClickListener {
+            binding.summeryCard.visibility = View.GONE
+            binding.editForm.visibility = View.VISIBLE
+        }
+    }
+
 
 }
