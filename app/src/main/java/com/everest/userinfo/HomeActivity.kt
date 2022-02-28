9aa54811d92a8852645e2201ed760cfeb6e2374b
wenkey <muni.v.g@everest.engineering>
2022-02-28 14:27:22 +0530

    Resolved confirugation changes

--- app/src/main/java/com/everest/userinfo/HomeActivity.kt
+++ app/src/main/java/com/everest/userinfo/HomeActivity.kt
@@ -1,41 +1,92 @@
 package com.everest.userinfo
 
+import address
+import addressCd
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.everest.userinfo.databinding.ActivityHomeBinding
 import com.everest.userinfo.validation.TextFieldValidation
+import email
+import emailCd
+import phone_number
+import phone_number_Cd
+import pin_code
+import pin_codeCd
+import summery_card
+import userName
+import userNameCd
+import user_form
 
 class HomeActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityHomeBinding
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        binding.validateButton.setOnClickListener {
-            if(isValidate()) {
-               onClick()
 
+        binding.validateButton.setOnClickListener {
+            if (isValidate()) {
+                showSummeryCardView()
             }
         }
 
     }
 
-    private fun isValidate(): Boolean {
-       return TextFieldValidation().validateUserName(binding, this) &&
-                TextFieldValidation().validateEmail(binding, this)
-                && TextFieldValidation().validatePhoneNumber(binding, this)
-               && TextFieldValidation().validatePinCode(binding, this)
-               && TextFieldValidation().validateAddress(binding, this)
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(user_form, binding.editForm.visibility)
+        outState.putInt(summery_card, binding.summeryCard.visibility)
+        super.onSaveInstanceState(outState)
+
+        outState.putString(userName, binding.nameId.text.toString())
+        outState.putString(email, binding.emailId.text.toString())
+        outState.putString(phone_number, binding.phoneNumberId.text.toString())
+        outState.putString(pin_code, binding.pinCodeId.text.toString())
+        outState.putString(address, binding.addressId.text.toString())
+
+        outState.putCharSequence(userNameCd, binding.userName.editText?.text.toString())
+        outState.putCharSequence(emailCd, binding.email.editText?.text.toString())
+        outState.putCharSequence(phone_number_Cd, binding.phoneNumber.editText?.text.toString())
+        outState.putCharSequence(pin_codeCd, binding.pinCode.editText?.text.toString())
+        outState.putCharSequence(addressCd, binding.address.editText?.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        binding.editForm.visibility = savedInstanceState.getInt(user_form, View.GONE)
+        binding.summeryCard.visibility = savedInstanceState.getInt(summery_card, View.VISIBLE)
+
+        super.onRestoreInstanceState(savedInstanceState)
+
+        binding.userName.editText?.setText(savedInstanceState.getCharSequence(userNameCd))
+        binding.email.editText?.setText(savedInstanceState.getCharSequence(emailCd))
+        binding.phoneNumber.editText?.setText(savedInstanceState.getCharSequence(phone_number_Cd))
+        binding.pinCode.editText?.setText(savedInstanceState.getCharSequence(pin_codeCd))
+        binding.address.editText?.setText(savedInstanceState.getCharSequence(addressCd))
+
+        binding.nameId.text = savedInstanceState.getString(userName)
+        binding.emailId.text = savedInstanceState.getString(email)
+        binding.phoneNumberId.text = savedInstanceState.getString(phone_number)
+        binding.pinCodeId.text = savedInstanceState.getString(pin_code)
+        binding.addressId.text = savedInstanceState.getString(address)
+
+        conformButton()
+        cancelButton()
+
     }
 
 
-    private fun onClick(){
+    private fun isValidate(): Boolean {
+        return TextFieldValidation().validateUserName(binding, this) &&
+                TextFieldValidation().validateEmail(binding, this)
+                && TextFieldValidation().validatePhoneNumber(binding, this)
+                && TextFieldValidation().validatePinCode(binding, this)
+                && TextFieldValidation().validateAddress(binding, this)
+    }
+    private fun showSummeryCardView() {
         binding.editForm.visibility = View.GONE
         binding.summeryCard.visibility = View.VISIBLE
         binding.nameId.text = binding.userName.editText?.text.toString()
@@ -44,16 +95,27 @@ class HomeActivity : AppCompatActivity() {
         binding.pinCodeId.text = binding.pinCode.editText?.text.toString()
         binding.addressId.text = binding.address.editText?.text.toString()
 
+        conformButton()
+
+        cancelButton()
+
+    }
+
+    private fun conformButton() {
         binding.confirmButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+    }
 
+
+    private fun cancelButton() {
         binding.cancelButton.setOnClickListener {
             binding.summeryCard.visibility = View.GONE
             binding.editForm.visibility = View.VISIBLE
         }
     }
+
 
 
 }
---
+++ app/src/main/java/com/everest/userinfo/constants/Constants.kt
@@ -0,0 +1,16 @@
+
+val userName= "USERNAME"
+val email = "EMAIl"
+val phone_number = "PHONE_NUMBER"
+val pin_code = "PIN_CODE"
+val address = "ADDRESS"
+val user_form = "USER_FORM"
+val summery_card = "SUMMERY_CARD"
+
+val userNameCd= "USER_NAME_CD"
+val emailCd = "EMAIl_CD"
+val phone_number_Cd = "PHONE_NUMBER_CD"
+val pin_codeCd = "PIN_CODE_CD"
+val addressCd = "ADDRESS_CD"
+val user_formCd = "USER_FORM_CD"
+val summery_cardCd = "SUMMERY_CARD_CD"
